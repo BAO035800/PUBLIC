af546df4dfa1e494099adde01c6209219045ef79
BAO035800 <baomoren50@gmail.com>
2025-05-04 01:09:06 +0700

    update

--- tesst/hello.py
+++ tesst/hello.py
@@ -1,5 +1,19 @@
 import cv2
+import os
+
+# Kiểm tra nếu thư mục "test" không tồn tại thì tạo
+if not os.path.exists("test"):
+    os.makedirs("test")
+
+# Mở webcam
 cap = cv2.VideoCapture(0)
+
+# Đọc một frame từ webcam
 ret, frame = cap.read()
-cv2.imwrite("bienso.jpg", frame)
+
+# Nếu có thể đọc được frame, lưu ảnh vào thư mục "test"
+if ret:
+    cv2.imwrite("test/OIP.jpg", frame)
+
+# Giải phóng webcam
 cap.release()
